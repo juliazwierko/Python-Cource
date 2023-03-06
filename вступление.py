f5753995d899d4ec41b34a4d0c6e1a1355280b61
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-03-06 23:16:20 +0100

    Update вступление.py

--- вступление.py
+++ вступление.py
@@ -52,3 +52,25 @@ while i < 10:
     print("\n")
     j = 1
     i += 1
+
+#6
+people = ["Tom", "Bob"]
+ 
+# добавляем в конец списка
+people.append("Alice")  # ["Tom", "Bob", "Alice"]
+# добавляем на вторую позицию
+people.insert(1, "Bill")  # ["Tom", "Bill", "Bob", "Alice"]
+# добавляем набор элементов ["Mike", "Sam"]
+people.extend(["Mike", "Sam"])      # ["Tom", "Bill", "Bob", "Alice", "Mike", "Sam"]
+# получаем индекс элемента
+index_of_tom = people.index("Tom")
+# удаляем по этому индексу
+removed_item = people.pop(index_of_tom)     # ["Bill", "Bob", "Alice", "Mike", "Sam"]
+# удаляем последний элемент
+last_item = people.pop()     # ["Bill", "Bob", "Alice", "Mike"]
+# удаляем элемент "Alice"
+people.remove("Alice")      # ["Bill", "Bob", "Mike"]
+print(people)       # ["Bill", "Bob", "Mike"]
+# удаляем все элементы
+people.clear()
+print(people)       # []
