667ffc7b5fc92a3ec5159a0e9c7811479d793876
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-03-09 23:39:27 +0100

    Update 01.c

--- Zadania/01.c
+++ Zadania/01.c
@@ -1,4 +1,12 @@
+#Есть список a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89].
+#Выведите все элементы, которые меньше 5.
+
+#Варианты решения:
+
 for elem in a:
     if elem < 5:
         print(elem)
-    
+            
+print(list(filter(lambda elem: elem < 5, a)))
+
+print([elem for elem in a if elem < 5])
