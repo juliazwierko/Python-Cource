eac7b808208e870b9ceb23185bda3f1d1a11d18e
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-03-09 23:27:35 +0100

    Create 01_Podstawowe_Operacje.py

---
+++ 01_Podstawowe_Operacje.py
@@ -0,0 +1,105 @@
+# 1
+print("1)Wynik: 5")
+print("wynik:",5)
+print(end="\n\n")
+
+#2
+print("2)Wynik: ",7,15,".")
+print("Wynik: ",7,15,".", sep="")
+print(end="\n\n")
+
+#3
+print("3)new line")
+print("Wynik: ",7,15,".", sep="", end="\n")
+print("Wynik: ",7,15,".", sep="", end="")
+print("nie ma spacji")
+print(end="\n\n")
+
+# 4 
+print("4)New \" line")
+print('New \" line') 
+print('New \' line')
+print(end="\n\n")
+
+#5
+print("5)Kocham cie,\nmoja\ncynamonka")
+print(end="\n\n")
+
+#6
+print("6)moroz\ti solnce\tdzen czudiesnyj")
+print("Lubie czekolade/t z oczersz")
+print("cos tam \\")
+print(end="\n\n")
+
+#7 Dzialania Matematyczne
+print('7)',5+5)
+print(5*5)
+print(5/5)
+print(5-5)
+# Potegowanie
+print(5**2)
+print(5**3)
+print(5/3)
+print(5//3)
+print(end="\n\n")
+
+#8 Funkcje Matematyczne
+print("Wynik:", min(1,5,8,34,5,78,-56,-3))
+print("Wynik:", max(1,5,8,34,5,78,-56,-3))
+# abs - modulo
+print("Wynik:", abs(-3))
+# "pow(a,b)" - a do potegi b 
+print("Wynik:", pow (5,2))
+# round -przecyzowanie 
+print("Wynik:", round(5,204998789))
+
+#9 input
+input("Wpisz swoj wiek: ")
+
+"""
+python3 program1.py
+1)Wynik: 5
+wynik: 5
+
+
+2)Wynik:  7 15 .
+Wynik: 715.
+
+
+3)new line
+Wynik: 715.
+Wynik: 715.nie ma spacji
+
+
+4)New " line
+New " line
+New ' line
+
+
+5)Kocham cie,
+moja
+cynamonka
+
+
+6)moroz i solnce        dzen czudiesnyj
+Lubie czekolade/t z oczersz
+cos tam \
+
+
+7) 10
+25
+1.0
+0
+25
+125
+1.6666666666666667
+1
+
+
+Wynik: -56
+Wynik: 78
+Wynik: 3
+Wynik: 25
+Wynik: 5
+Wpisz swoj wiek:18
+"""
