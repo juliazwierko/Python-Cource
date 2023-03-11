e9b7758114eb05575d2070f5156f7438ebb56e4e
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-03-11 20:30:11 +0100

    Update 02_Zmienne_i_Typy_danych.py

--- Teoria/02_Zmienne_i_Typy_danych.py
+++ Teoria/02_Zmienne_i_Typy_danych.py
@@ -20,3 +20,25 @@ print("\n")
 
 #Funkcja str(float/int) - float/int-->string
 #Funkcja int(string) - string-->int
+
+#unput() - praca z uzytkownikiem 
+num_1 = int(input("Wpisz 1 liczbe "))
+num_2 = int(input("Wpisz 2 liczbe "))
+
+print("Dzialania matematyczny na wpisanych liczbach:")
+print("num_1 + num_2 = ", num_1+num_2)
+print("num_1 - num_2 = ", num_1-num_2)
+print("num_1 * num_2 = ", num_1*num_2)
+print("num_1^num_2 = ", num_1**num_2)
+print("num_1 / num_2 = ", num_1/num_2)
+print("num_1 // num_2 = ", num_1//num_2, end="\n\n")
+
+num_1 += 5 #num_1 = num_1+5
+print("num_1 + 5 = ",num_1, end="\n\n")
+
+word = "Paweeeł <3" #string
+print(word * 2) #wypisuje 2 razy
+word = "Cynamonka ^_^" #string
+print(word)
+word = True #bool
+print(word)
