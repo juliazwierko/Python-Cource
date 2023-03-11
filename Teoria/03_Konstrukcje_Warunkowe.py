ef74a6d108befd03f175616db59a7771e500756f
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-03-11 15:03:24 +0100

    Create 03_Konstrukcje_Warunkowe.py

---
+++ Teoria/03_Konstrukcje_Warunkowe.py
@@ -0,0 +1,16 @@
+if 5 == 5:
+    print("5 = 5")
+    print("!!!", end="\n\n")
+
+if 5 > 5:
+    print("5 > 5")
+
+user_data = int(input("Wpisz liczbe: "))
+print("Podana liczba", user_data, end="\n\n")
+
+if user_data != 5:
+    print("liczba weksza od 5")
+    # < > =< +> == !=   
+    if user_data == 6:
+        print("Liczba jest rowna 6")
+    
